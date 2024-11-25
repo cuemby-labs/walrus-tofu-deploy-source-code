08089a8a3a5be01907cd68a1fecf3f9ae5f057c9
Manuel Nuila <manuel.nuila@cuemby.com>
2024-11-24 21:37:05 -0600

    update workflow

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -2,7 +2,21 @@ resource "kubernetes_ingress_v1" "ingress" {
   metadata {
     name        = var.app_name
     namespace   = var.app_namespace
-    annotations = var.annotations
+    annotations = merge(
+      {
+        "nginx.ingress.kubernetes.io/server-snippet" = <<-EOT
+          location ~ /\.git { 
+            deny all; 
+            return 404; 
+          }
+          location = /Dockerfile {
+            deny all;
+            return 404;
+          }
+        EOT
+      },
+      var.annotations
+    )
     labels      = local.labels
   }
 
