f9f5c22c01b0694bbbe25741da60b02769f3e364
andres06-hub <andressierrarojas7@gmail.com>
2025-03-20 15:44:41 -0500

    refactor: kaniko image context

--- main.tf
+++ main.tf
@@ -2,29 +2,30 @@
 # Build
 #######
 
-resource "null_resource" "checkout_commit" {
-  count = var.git_commit != "" ? 1 : 0
+# resource "null_resource" "checkout_commit" {
+#   count = var.git_commit != "" ? 1 : 0
 
-  provisioner "local-exec" {
-    command = <<EOT
-    mkdir -p /tmp/repo
-    git clone --depth=100 --branch=${var.git_branch} ${var.git_url} /tmp/repo
-    ls -la /tmp/repo
-    $( cd /tmp/repo && git checkout ${var.git_commit} )
+#   provisioner "local-exec" {
+#     command = <<EOT
+#     mkdir -/tmp
+#     git clone --depth=100 --branch=${var.git_branch} ${var.git_url} /tmp/repo
+#     ls -la /tmp/repo
+#     $( cd /tmp/repo && git checkout ${var.git_commit} )
 
-    tar -C /tmp/repo -zcvf /tmp/context.tar.gz .
-    ls -la /tmp/context.tar.gz
-    EOT
-  }
-}
+#     tar -C /tmp/repo -zcvf /tmp/context.tar.gz .
+#     ls -la /tmp/context.tar.gz
+#     EOT
+#   }
+# }
 
 resource "kaniko_image" "image" {
   # Context: use tag if provided, otherwise use branch
   # context = "${local.formal_git_url}#${var.git_commit != "" ? var.git_commit : (var.git_tag != "" ? "refs/tags/${var.git_tag}" : "refs/heads/${var.git_branch}")}"
   # context=git://<git-repo-url>/<git-repo-path>#refs/heads/<branch name>#<commit-id>
-  context     = var.git_commit != "" ? "tar://tmp/context.tar.gz" : "${local.formal_git_url}#${var.git_tag != "" ? "refs/tags/${var.git_tag}" : "refs/heads/${var.git_branch}"}"
+  # context     = var.git_commit != "" ? "tar://tmp/context.tar.gz" : "${local.formal_git_url}#${var.git_tag != "" ? "refs/tags/${var.git_tag}" : "refs/heads/${var.git_branch}"}"
+  context     = var.git_commit != "" ? "${local.formal_git_url}#refs/heads/${var.git_branch}#${var.git_commit}" : "${local.formal_git_url}#${var.git_tag != "" ? "refs/tags/${var.git_tag}" : "refs/heads/${var.git_branch}"}"
 
-  depends_on = [null_resource.checkout_commit]
+  # depends_on = [null_resource.checkout_commit]
 
   dockerfile  = var.dockerfile
   # dockerfile = var.git_commit != "" ? "/tmp/repo/${replace(var.dockerfile, "./", "")}" : var.dockerfile
