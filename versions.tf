1ff6176fc9df2cb1a0ef1696c924c0ebff928457
Manuel Nuila <manuel.nuila@cuemby.com>
2025-05-05 13:56:07 -0600

    test tofu template

--- versions.tf
+++ versions.tf
@@ -2,26 +2,22 @@ terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
-    knative = {
-      source = "theomessin/knative"
-      version = "0.1.1"
-    }
     kaniko = {
       source  = "seal-io/kaniko"
       version = "0.0.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.32.0"
+      version = "2.36.0"
     }
     template = {
       source  = "hashicorp/template"
       version = ">= 2.2.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
+    # kubectl = {
+    #   source  = "gavinbunney/kubectl"
+    #   version = ">= 1.14.0"
+    # }
     null = {
       source  = "hashicorp/null"
       version = "3.2.3"
@@ -34,9 +30,5 @@ terraform {
       source = "hashicorp/local"
       version = "~> 2.0"
     }
-    # knative = {
-    #   source = "theomessin/knative"
-    #   version = "0.1.1"
-    # }
   }
 }
